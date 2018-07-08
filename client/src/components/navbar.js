22296bbfbcd85d53aeef176ed3b2f314bf889977
Wyatt Sommer <wyatt@hampton.digital>
2018-07-08 16:48:49 -0600

    two

---
+++ client/src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+
+    render() {
+        return (
+            <div className="navbar">
+                <h1>News4You</h1>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
